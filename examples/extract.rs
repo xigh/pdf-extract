bce34ff236811403bc4657e64e581431c3165278
Philippe Anel <zexigh@gmail.com>
2024-09-30 16:19:27 +0200

    small example fixes

--- examples/extract.rs
+++ examples/extract.rs
@@ -7,22 +7,28 @@ use std::path;
 use std::io::BufWriter;
 use std::fs::File;
 use pdf_extract::*;
-use lopdf::*;
 
-fn main() {
+fn main() -> std::result::Result<(), OutputError> {
     //let output_kind = "html";
     //let output_kind = "txt";
     //let output_kind = "svg";
-    let file = env::args().nth(1).unwrap();
+    let Some(file) = env::args().nth(1) else {
+        println!("Usage: {} <pdf_file> <output_kind>", env::args().nth(0).unwrap());
+        return Ok(());
+    };
     let output_kind = env::args().nth(2).unwrap_or_else(|| "txt".to_owned());
-    println!("{}", file);
     let path = path::Path::new(&file);
     let filename = path.file_name().expect("expected a filename");
     let mut output_file = PathBuf::new();
     output_file.push(filename);
     output_file.set_extension(&output_kind);
+    println!("{} -> {:?}", file, output_file);
+
     let mut output_file = BufWriter::new(File::create(output_file).expect("could not create output"));
-    let doc = Document::load(path).unwrap();
+    let Ok(doc) = Document::load(&path) else {
+        eprintln!("Could not load document {:?}", path);
+        return Ok(());
+    };
 
     print_metadata(&doc);
 
@@ -33,5 +39,5 @@ fn main() {
         _ => panic!(),
     };
 
-    output_doc(&doc, output.as_mut());
+    output_doc(&doc, output.as_mut())
 }
